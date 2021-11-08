34eab42877f0c45b28e850803d186c53cf798b67
jiangranwang <wangjiangran3@gmail.com>
2021-11-08 00:18:29 -0600

    clean

--- mysite/homePage/views.py
+++ mysite/homePage/views.py
@@ -1,26 +1,12 @@
 from django.shortcuts import render
-
-# from pathlib import Path
-# import sys
-  
-# # directory reach
-# directory = Path(__file__).resolve()
-  
-# # setting path
-# sys.path.append(directory.parent.parent)
-  
-# importing
-# import models
-# from ..models import *
-  
-# using
 from .models import Stock   
 
 
 def homepage_view(request):
-    if request.POST['submit'] == 'searchKeyword':
-            keyword = request.POST['keyword']
-            queryset = models.Stock.objects.get(stockname__contains = keyword)
-            context = {'queryset': queryset}
-            return render(request, 'homepage/homePage.html', context)
+    if request.method == 'POST':
+        if request.POST['submit'] == 'searchKeyword':
+                keyword = request.POST['keyword']
+                queryset = Stock.objects.get(stockname__contains = keyword)
+                context = {'queryset': queryset}
+                return render(request, 'homepage/homePage.html', context)
 
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'accounts',
     'stock',
     'watchlist',
-    'models.chat',
 ]
 
 MIDDLEWARE = [
