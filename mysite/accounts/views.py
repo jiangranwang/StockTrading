292b4ce5cf4a69e7fd6df1a1bcd6fe523e3a8d98
jiangranwang <wangjiangran3@gmail.com>
2021-11-08 23:37:41 -0600

    add advance query

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
-from .forms import CreateUserForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import PasswordChangeForm
+from django.db import connection
+from .forms import CreateUserForm
+from .models import Stock
 
 
 # Create your views here.
 @login_required(login_url='login')
 def home_view(request):
+    watchlist = Stock.objects.raw('select * from Stock where stockid in '
+                                  '(select stockid from Watchlist where UserId = '
+                                  '(select id from auth_user where username = %s))', [request.user.username])
+    cursor = connection.cursor()
+    cursor.execute('select stockname, freq from Stock natural join '
+                   '(select count(userid) as freq, stockid from Watchlist '
+                   'group by stockid having stockid in '
+                   '(select stockid from Watchlist where UserId = '
+                   '(select id from auth_user where username = %s))) a '
+                   'order by freq desc, stockname asc', [request.user.username])
+    rows = cursor.fetchall()
+    freqs = []
+    for r in rows:
+        freqs.append({'stockname': r[0], 'freq': r[1]})
+    return render(request, 'accounts/home.html', {'watchlist': watchlist, 'freqs': freqs})
+
+
+@login_required(login_url='login')
+def update_password(request):
     changeForm = PasswordChangeForm(request.user)
     if request.method == 'POST':
         changeForm = PasswordChangeForm(request.user, request.POST)
@@ -17,7 +38,7 @@ def home_view(request):
             update_session_auth_hash(request, user)
             messages.success(request, 'Password successfully changed')
             return redirect('logout')
-    return render(request, 'accounts/home.html', {'changeForm': changeForm})
+    return render(request, 'accounts/update_password.html', {'changeForm': changeForm})
 
 
 def delete_user(request):
