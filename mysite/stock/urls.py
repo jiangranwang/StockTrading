74d0f13b57a2e64852601fe0ec218654a95b4810
GloomySunday2000 <liushenjiang2524445402@gmail.com>
2021-11-24 01:52:11 -0600

    finished setting up search to single page

--- mysite/stock/urls.py
+++ mysite/stock/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
+    path('index', views.Index.as_view(), name='index'),
+    path('layout-static', views.layout_static, name='layout-static'),
+    path('layout-sidenav-light', views.layout_sidenav_light, name='layout-sidenav-light'),
+    path('<StockName>', views.stock_graph, name='stock-graph'),
+
 ]
+
+
--- mysite/stock/views.py
+++ mysite/stock/views.py
@@ -3,10 +3,27 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.views import View
-
+from .models import Stock
 
 class Index(View):
     template = 'stock/index.html'
 
     def get(self, request):
         return render(request, self.template)
+    
+
+
+def stock_graph(request, StockName):
+    stock = Stock.objects.get(stockname=StockName)
+    return render(request, 'stock/stock-graph.html',{'StockName': stock})
+    
+
+def layout_sidenav_light(request):
+    print(request.method)
+    
+    return render(request, 'stock/layout-sidenav-light.html')
+
+def layout_static(request):
+    print(request.method)
+    
+    return render(request, 'stock/layout-static.html')
